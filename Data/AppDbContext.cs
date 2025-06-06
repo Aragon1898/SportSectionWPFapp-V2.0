d7413372d2d1be707a04f0f2a1e06f009b7e0a15
Aragon1898 <148039800+Aragon1898@users.noreply.github.com>
2025-06-06 15:56:14 +0300

    Add files via upload

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,37 @@
+﻿using SportSectionWPF2.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SportSectionWPF2.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext() : base("SportSectionDB")
+        {
+
+        }
+
+        public DbSet<UserEntity> Users { get; set; }
+        public DbSet<AchievementEntity> Achievements { get; set; }
+        public DbSet<CoachEntity> Coachs { get; set; }
+        public DbSet<AttendancesEntity> Attendances { get; set; }
+        public DbSet<MemberEntity> Members { get; set; }
+        public DbSet<ScheduleEntity> Schedules { get; set; }
+        public DbSet<SectionEntity> Sections { get; set; }
+        public DbSet<AdminEntity> Admins { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<SectionEntity>()
+                .HasOptional(s => s.Schedule)
+                .WithRequired(sch => sch.Section);
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+    }
+}
